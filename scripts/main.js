eeaa6ddf425c6808273ae67bb7c8ee10c3b38d46
joanbaldo <joanbaldo@hotmail.com>
2023-02-20 11:10:16 +0100

    Variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-console.log('Joan');
+//objetos//
+const person = {  name: "Edu",  age: 30,  hobbies: ["music", "movies", ["futbol",  {    name:"tenis",    team:["individual","por parejas"]  }  , "baloncesto"]],  address: {    street: "50 Main st",    city: "Boston",    state: "MA",  },}; 
+
+console.log(person.hobbies[2][1].team[0])
